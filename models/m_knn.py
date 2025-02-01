296c3b81672d9db0b5c266b7d98e4844f04a9ab5
peppioi <giuseppe.lioi.lgp@gmail.com>
2025-02-01 02:45:15 +0100

    Modifica classe KNNClassifier derivata dalla classe padre Classifier

--- models/m_knn.py
+++ models/m_knn.py
@@ -20,61 +20,49 @@ class Classifier(ABC):
         """Metodo per fare previsioni."""
         pass
 
-class KNNClassifier:
-    def __init__(self, k=3, num_folds=5):
-        """
-        Inizializza il classificatore KNN.
-        
-        :param k: Numero di vicini da considerare.
-        :param num_folds: Numero di fold per la validazione incrociata.
-        """
+class KNNClassifier(Classifier):
+    """
+    Implementazione del classificatore k-NN (k-Nearest Neighbors) da zero.
+    """
+    def __init__(self, k=3):
+        """Inizializza il modello con il numero di vicini k."""
         self.k = k
-        self.num_folds = num_folds
-
-    @staticmethod
-    def euclidean_distance(x1, x2):
-        """
-        Calcola la distanza euclidea tra due punti.
-        
-        :param x1: Primo punto.
-        :param x2: Secondo punto.
-        :return: Distanza euclidea.
-        """
-        return np.sqrt(np.sum((x1 - x2) ** 2))
+        self.X_train = None
+        self.y_train = None
     
-    def classify(self, X_train, y_train, X_test):
-            """
-            Classifica i punti di test basandosi sui k vicini più vicini.
-            
-            :param X_train: Dati di addestramento.
-            :param y_train: Etichette di addestramento.
-            :param X_test: Dati di test.
-            :return: Array con le predizioni.
-            """
-            predictions = []
-            
-            for test_point in X_test:
-                # Calcolo delle distanze tra il punto di test e tutti i punti di training
-                distances = [self.euclidean_distance(test_point, x) for x in X_train]
-                
-                # Ottieni gli indici dei k punti più vicini
-                k_indices = np.argsort(distances)[:self.k]
-                
-                # Ottieni le etichette dei k vicini
-                k_nearest_labels = [y_train[i] for i in k_indices]
-                
-                # Conta le occorrenze di ogni classe tra i vicini
-                most_common = Counter(k_nearest_labels).most_common()
-                
-                # Se c'è un pareggio, sceglie casualmente tra le classi più frequenti
-                if len(most_common) > 1 and most_common[0][1] == most_common[1][1]:
-                    prediction = random.choice([most_common[0][0], most_common[1][0]])
-                else:
-                    prediction = most_common[0][0]
-                
-                predictions.append(prediction)
-            
-            return np.array(predictions)
+    def fit(self, X, y):
+        """Memorizza i dati di training."""
+        self.X_train = np.array(X)
+        self.y_train = np.array(y)
+    
+    def predict(self, X_test):
+        """Effettua una previsione su un insieme di test. Utilizza il metodo privato _predict_single() in modo iterativo"""
+        X_test = np.array(X_test)
+        predictions = [self._predict_single(x) for x in X_test]
+        return np.array(predictions)
+    
+    def _predict_single(self, x):
+        """Effettua una previsione per un singolo punto usando la distanza euclidea."""
+        # Calcola la distanza euclidea tra x e tutti i punti del training set
+        distances = np.linalg.norm(self.X_train - x, axis=1)
+        
+        # Trova gli indici dei k vicini più vicini andando a prendere i primi k elementi dell'array k_indices
+        k_indices = np.argsort(distances)[:self.k]
+        
+        # Ottiene le etichette dei k vicini
+        k_nearest_labels = self.y_train[k_indices]
+        
+        # Determina la classe più frequente tra i vicini
+        most_common = Counter(k_nearest_labels).most_common(2)
+        
+        # Se c'è un pareggio, sceglie casualmente tra le classi più frequenti
+        if len(most_common) > 1 and most_common[0][1] == most_common[1][1]:
+            prediction = random.choice([most_common[0][0], most_common[1][0]])
+        else:
+            prediction = most_common[0][0]
+        
+        return prediction
+
     
     
     @staticmethod
