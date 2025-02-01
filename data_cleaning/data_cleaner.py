2e758f1b7be2cf8d7d0864076d75595ba1008528
Damiano <damiano.caputo9498@gmail.com>
2025-02-01 17:18:56 +0100

    aggiunto il file che conterrà la normalizzazione e ho aggiunto la classe SaveDB a data_cleaner per poter avere il db pulito salvato

--- data_cleaning/data_cleaner.py
+++ data_cleaning/data_cleaner.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import os 
 
 class RimuoviDuplicati:
     """
@@ -49,6 +50,30 @@ class ValoriMancanti:
         Sostituisce i valori mancanti con la deviazione standard della colonna.
         """
         return data.fillna(data.median())
+    
+class SaveDB:
+    @staticmethod
+    def save_dataset(data: pd.DataFrame):
+        save_option = input("\nVuoi salvare il dataset pulito? (s/n): ").strip().lower()
+        if save_option == 's':
+            default_folder = "data/cleaned"  # Cartella di default
+            os.makedirs(default_folder, exist_ok=True)  # Crea la cartella se non esiste
+
+            output_filename = input(f"Inserisci il nome del file di output (lascia vuoto per 'cleaned_data.csv'): ").strip()
+
+            # Se non viene specificato un nome, usa "cleaned_data.csv"
+            if not output_filename:
+                output_filename = "cleaned_data.csv"
+
+            # Costruisci il percorso completo (relativo)
+            relative_path = os.path.join(default_folder, output_filename)
+
+            # Converti in percorso assoluto
+            absolute_path = os.path.abspath(relative_path)
+
+            # Salva il file
+            data.to_csv(absolute_path, index=False)
+            print(f"Dataset pulito salvato in: {absolute_path}")
     
 class GestioneValMancanti:
     """
