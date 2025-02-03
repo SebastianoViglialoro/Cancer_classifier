ec328a02e76956008ac1811784cd42b9f6718b92
Damiano <damiano.caputo9498@gmail.com>
2025-02-03 16:43:10 +0100

    modifca nel main.py con il nuovo metodo di import del dataset da parte dell'utente, riducendo quindi la complessità del file stesso

--- main.py
+++ main.py
@@ -7,20 +7,7 @@ from evaluation import ModelEvaluation
 
 def main():
     #Input dell'utente per l'import del file
-    ok = False
-    while not ok:
-        file_path = input("Inserisci il percorso del file del dataset di analisi: ").strip()
-        if not file_path:
-            print("Il percorso non può essere vuoto. Riprova.")
-            continue  # Torna all'inizio del ciclo
-        print("Import del dataset in corso...")
-
-        try:
-            importer = SelectionFile.get_file(file_path)
-            data = importer.importer(file_path)
-            break  # Esce dal loop solo se il file viene caricato correttamente
-        except Exception as e:
-            print(f"Errore durante l'import del file: \n{e}. Riprova.")
+    data = SelectionFile.import_data()
 
     if data.empty: #se il dataset è vuoto, termina l'esecuzione e arresta il sistema
         print("Il dataset è vuoto. Termino l'esecuzione.")
