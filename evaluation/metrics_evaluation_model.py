918410eb24e7d560dad90e91ecbeb835b81d13a2
peppioi <giuseppe.lioi.lgp@gmail.com>
2025-02-04 20:09:10 +0100

    Eliminazione importazione ridondante

--- evaluation/metrics_evaluation_model.py
+++ evaluation/metrics_evaluation_model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 # Questa classe si occupa della valutazione di un modello di classificazione.
 # Include metriche di valutazione, validazione incrociata e strumenti di analisi visiva.
